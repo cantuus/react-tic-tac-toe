2c9948effc5c7dd8bfd6738fd7850709dfc375b2
Sophia Koeut <koeutsophia@gmail.com>
2020-07-24 18:28:59 -0700

    properly .map through each square on the table

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
 const Board = ({ squares, onClick }) => (
-    <div>
-        <Square value="1" onClick={() => onClick("dumy value")} />
-        <Square value="2" onClick={() => onClick("dumy value")} />
-        <Square value="3" onClick={() => onClick("dumy value")} />
-        <Square value="4" onClick={() => onClick("dumy value")} />
-        <Square value="5" onClick={() => onClick("dumy value")} />
-        <Square value="6" onClick={() => onClick("dumy value")} />
-        <Square value="7" onClick={() => onClick("dumy value")} />
-        <Square value="8" onClick={() => onClick("dumy value")} />
-        <Square value="9" onClick={() => onClick("dumy value")} />
+    <div style={style}>
+        {squares.map((square, i) => (
+            <Square key={i} value={square} onClick={() => onClick(i)} />
+        ))}
     </div>
 )
 

66d635f1cb358620fdc02b0c5f2576805d7b5bef
Sophia Koeut <koeutsophia@gmail.com>
2020-07-24 17:51:15 -0700

    added Board, Game, Square components with little styling to Board

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = ({ squares, onClick }) => (
+    <div>
+        <Square value="1" onClick={() => onClick("dumy value")} />
+        <Square value="2" onClick={() => onClick("dumy value")} />
+        <Square value="3" onClick={() => onClick("dumy value")} />
+        <Square value="4" onClick={() => onClick("dumy value")} />
+        <Square value="5" onClick={() => onClick("dumy value")} />
+        <Square value="6" onClick={() => onClick("dumy value")} />
+        <Square value="7" onClick={() => onClick("dumy value")} />
+        <Square value="8" onClick={() => onClick("dumy value")} />
+        <Square value="9" onClick={() => onClick("dumy value")} />
+    </div>
+)
+
+
+export default Board
---
+++ src/components/Game.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Board from './Board'
+
+const Game = () => {
+
+    const handleClick = () => {
+
+    }
+
+    const jumptTo = () => {
+
+    }
+
+    const renderMoves = () => {
+
+    }
+
+
+    return (
+        <Board onClick={handleClick} />
+    )
+}
+
+export default Game;
